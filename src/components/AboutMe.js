e41de49b7210d19a7c7e6791908547d90a827f06
My Name <ben.d.maryland@gmail.com>
2022-06-10 18:47:53 -0400

    styling getting close

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,37 +3,37 @@ import React from 'react'
 function AboutMe() {
   return (
     <div>
-      <div class="about" id="about">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-12 col-md-12 text-center">
-              <div class="about-header">
+      <div className="about" id="about">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-12 col-md-12 text-center">
+              <div className="about-header">
                 <h1>About Me</h1>
               </div>
             </div>
           </div>
 
-          <div class="row">
-            <div class="col-sm-12 col-md-12 text-center">
-              <div class="about-header-quote">
+          <div className="row">
+            <div className="col-sm-12 col-md-12 text-center">
+              <div className="about-header-quote">
                 <h3>Sharing a little bit of my story...</h3>
               </div>
             </div>
           </div>
 
-          <div class="row">
-            <div class="about-general-description ">
-              <div class="col-sm-8 col-md-8 ">
+          <div className="row">
+            <div className="about-general-description ">
+              <div className="col-sm-8 col-md-8 ">
                 <h2>General Overview</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
               </div>
             </div>
           </div>
 
-          <div class="row">
-            <div class="statistic-row">
-              <div class="about-statistic-description">
-                <div class="col-sm-8 col-md-8">
+          <div className="row">
+            <div className="statistic-row">
+              <div className="about-statistic-description">
+                <div className="col-sm-8 col-md-8">
                   <h2>Statistics</h2>
                   <h5><em>And for those that don't fancy reading:</em></h5>
 
@@ -48,10 +48,10 @@ function AboutMe() {
             </div>
           </div>
 
-          <div class="row">
-            <div class="about-option-buttons">
-              <div class="col-sm-2 col-md-2">
-                <a href="#contact" class="btn btn-default btn-black-border">Contact</a>
+          <div className="row">
+            <div className="about-option-buttons">
+              <div className="col-sm-2 col-md-2">
+                <a href="#contact" className="btn btn-default btn-black-border">Contact</a>
               </div>
             </div>
           </div>
@@ -60,33 +60,33 @@ function AboutMe() {
         </div>
       </div >
 
-      <div class="summary">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="development-summary">
-              <div class="col-sm-1 col-md-1">
-                <div class="development-img">
-                  <img class="img-responsive" src="./images/developerdesign.svg"/>
+      <div className="summary">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="development-summary">
+              <div className="col-sm-1 col-md-1">
+                <div className="development-img">
+                  <img className="img-responsive" src="./images/developerdesign.svg"/>
                 </div>
               </div>
 
-              <div class="col-sm-4 col-md-4">
-                <div class="development-description">
+              <div className="col-sm-4 col-md-4">
+                <div className="development-description">
                   <h3>Development and Design</h3>
                   <p>I aim to put my creativity to the test, designing and building unique, meaningful products for clients or merely for my own interests.</p>
                 </div>
               </div>
             </div>
 
-            <div class="responsive-summary">
-              <div class="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
-                <div class="responsive-img">
-                  <img class="img-responsive" src="./images/responsivedesign.svg"/>
+            <div className="responsive-summary">
+              <div className="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
+                <div className="responsive-img">
+                  <img className="img-responsive" src="./images/responsivedesign.svg"/>
                 </div>
               </div>
 
-              <div class="col-sm-4 col-md-4">
-                <div class="responsive-description">
+              <div className="col-sm-4 col-md-4">
+                <div className="responsive-description">
                   <h3>Responsive Layouts</h3>
                   <p>Development and design isn't merely putting information on the site or preferred media outlet. I organize content and present information in an engaging fashion, driving new and unique layouts in tandem with novel solutions and cool animations.</p>
                 </div>
@@ -94,29 +94,29 @@ function AboutMe() {
             </div>
           </div>
 
-          <div class="row">
-            <div class="ideas-summary">
-              <div class="col-sm-1 col-md-1">
-                <div class="idea-img">
-                  <img class="img-responsive" src="./images/innovativesolutions.svg"/>
+          <div className="row">
+            <div className="ideas-summary">
+              <div className="col-sm-1 col-md-1">
+                <div className="idea-img">
+                  <img className="img-responsive" src="./images/innovativesolutions.svg"/>
                 </div>
               </div>
 
-              <div class="col-sm-4 col-md-4">
-                <div class="idea-description">
+              <div className="col-sm-4 col-md-4">
+                <div className="idea-description">
                   <h3>Ideas and Solutions</h3>
                   <p>There are still many problems that exist in today's society, including laziness. Luckily, I hope to combat these issues by innovating, developing easy-to-use programs, solutions, or products.</p>
                 </div>
               </div>
             </div>
 
-            <div class="passion-summary">
-              <div class="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
-                <img class="img-responsive" src="./images/passion.svg"/>
+            <div className="passion-summary">
+              <div className="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
+                <img className="img-responsive" src="./images/passion.svg"/>
               </div>
 
-              <div class="col-sm-4 col-md-4">
-                <div class="passion-description">
+              <div className="col-sm-4 col-md-4">
+                <div className="passion-description">
                   <h3>Passion and Dedication</h3>
                   <p>With my profound interest and commitment to my field of study, my projects rarely go unfinished and my problems are never left unresolved. </p>
                 </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,31 @@
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 function Navbar() {
     return (
-        <div>
-            <Link className="nav-link" to="/"><button color="inherit">Home</button></Link>
-            <Link className="nav-link" to="/portfolio"><button color="inherit">Portfolio</button></Link>
-            <Link className="nav-link" to="/contacts"><button color="inherit">Contacts</button></Link>
+
+        <div className="navigation">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
+                        <div className="logo">
+                            <Link to="index.html"><img src="images/logo.svg"/></Link>
+                        </div>
+                    </div>
+
+                    <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
+                        <div className="primary-nav">
+                            <ul>
+                                <Link className="nav-link" to="/">Home</Link>
+                                <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                                <Link className="nav-link" to="/contacts">Contacts</Link>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
     )
 }
 
