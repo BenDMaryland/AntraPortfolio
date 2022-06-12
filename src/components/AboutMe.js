a0d09ad6f0f248272e058791b373e0687292dfd2
My Name <ben.d.maryland@gmail.com>
2022-06-12 02:04:14 -0400

    Getting close

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import DeveloperDesign from "../images/developerdesign.svg"
+import ResponsiveDesign from "../images/responsivedesign.svg"
+import InnovativeSolutions from "../images/innovativesolutions.svg"
+
+import Passion from "../images/passion.svg"
+
+
+
 
 function AboutMe() {
   return (
@@ -66,7 +74,7 @@ function AboutMe() {
             <div className="development-summary">
               <div className="col-sm-1 col-md-1">
                 <div className="development-img">
-                  <img className="img-responsive" src="./images/developerdesign.svg"/>
+                  <img className="img-responsive" alt='Developer Design' src={DeveloperDesign} />
                 </div>
               </div>
 
@@ -81,7 +89,7 @@ function AboutMe() {
             <div className="responsive-summary">
               <div className="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
                 <div className="responsive-img">
-                  <img className="img-responsive" src="./images/responsivedesign.svg"/>
+                  <img className="img-responsive" alt='Responsive Design'  src={ResponsiveDesign} />
                 </div>
               </div>
 
@@ -98,7 +106,7 @@ function AboutMe() {
             <div className="ideas-summary">
               <div className="col-sm-1 col-md-1">
                 <div className="idea-img">
-                  <img className="img-responsive" src="./images/innovativesolutions.svg"/>
+                  <img className="img-responsive" alt='Innovative Solutions ' src={InnovativeSolutions} />
                 </div>
               </div>
 
@@ -112,7 +120,7 @@ function AboutMe() {
 
             <div className="passion-summary">
               <div className="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
-                <img className="img-responsive" src="./images/passion.svg"/>
+                <img className="img-responsive" alt='Passion  '  src={Passion} />
               </div>
 
               <div className="col-sm-4 col-md-4">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 
 import { Link } from "react-router-dom"
+import Logo from "../images/logo.svg"
 function Navbar() {
     return (
 
@@ -8,16 +9,16 @@ function Navbar() {
                 <div className="row">
                     <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
                         <div className="logo">
-                            <Link to="index.html"><img src="images/logo.svg"/></Link>
+                            <Link to="index.html"><img src={Logo} alt='logo' /></Link>
                         </div>
                     </div>
 
                     <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
                         <div className="primary-nav">
                             <ul>
-                                <Link className="nav-link" to="/">Home</Link>
-                                <Link className="nav-link" to="/portfolio">Portfolio</Link>
-                                <Link className="nav-link" to="/contacts">Contacts</Link>
+                                <li>   <Link className="nav-link" to="/">Home</Link>	</li>
+                                <li>    <Link className="nav-link" to="/portfolio">Portfolio</Link>	</li>
+                                <li>     <Link className="nav-link" to="/contacts">Contacts</Link>	</li>
                             </ul>
                         </div>
                     </div>
